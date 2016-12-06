6154383417253de12ae6254ac7dc6645eee282bf
RichardZhang <richardzhang@apintec.com>
2016-12-06 19:45:32 +0800

    1. add bootstapper.

---
+++ PrismTest/PrismTestBootStapper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Prism.Mef;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrismTest
+{
+    class PrismTestBootStapper: MefBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<Shell>();
+        }
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow = Shell as Window;
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
