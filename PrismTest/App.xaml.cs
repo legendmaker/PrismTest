af741e45169253c716d3d5b4cc4f5b2e3020a1ea
RichardZhang <richardzhang@apintec.com>
2016-12-07 19:49:42 +0800

    1. bootstrapper based on MEF.

--- PrismTest/App.xaml.cs
+++ PrismTest/App.xaml.cs
@@ -13,5 +13,17 @@ namespace PrismTest
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            PrismTestBootStrapper bootStrapper = new PrismTestBootStrapper();
+            bootStrapper.Run();
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "启动错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            Environment.Exit(1);
+        }
     }
 }
---
+++ PrismTest/PrismTestBootStrapper.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Prism.Mef;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Modularity;
+
+namespace PrismTest
+{
+    class PrismTestBootStrapper: MefBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            //return Container.GetExportedValue<Shell>();
+            return ServiceLocator.Current.GetInstance<Shell>();
+        }
+        protected override void InitializeModules()
+        {
+            base.InitializeModules();
+            Application.Current.MainWindow = Shell as Shell;
+            Application.Current.MainWindow.Show();
+        }
+        protected override void ConfigureAggregateCatalog()
+        {
+            base.ConfigureAggregateCatalog();
+            AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(PrismTestBootStrapper).Assembly));
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            //return base.CreateModuleCatalog();
+            return new ConfigurationModuleCatalog();
+        }
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+        }
+        protected override void RegisterBootstrapperProvidedTypes()
+        {
+            base.RegisterBootstrapperProvidedTypes();
+        }
+    }
+}
